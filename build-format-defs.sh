3a7b39060d430a7172922774bc819b6951e455ac
GeffDev <geffgeffdev@gmail.com>
2023-09-23 17:49:36 +0800

    Fix build-format-defs.sh on linux

--- build-format-defs.sh
+++ build-format-defs.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 for file in ./Cosmic.Formats/defs/*.ksy; do
-    ./kaitai-struct-compiler-0.10/bin/kaitai-struct-compiler.bat -t csharp --outdir ./Cosmic.Formats/gen --dotnet-namespace Cosmic.Formats -I ./Cosmic.Core/Formats/gen $file.FullName
+    ./kaitai-struct-compiler-0.10/bin/kaitai-struct-compiler -t csharp --outdir ./Cosmic.Formats/gen --dotnet-namespace Cosmic.Formats -I ./Cosmic.Core/Formats/gen $file
 done
